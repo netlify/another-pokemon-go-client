f1dc87a72be075f1cbecc080db91a0fadcf44341
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-07-13 22:53:52 -0700

    styling and more clean ups

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-  extends: 'rackt',
+  extends: "rackt",
   plugins: [
-    'react',
+    "react",
   ],
   rules: {
-    'semi': [1, 'always'],                   // Use semicolons
-    'comma-dangle': [1, 'always-multiline'], // Use semicolons
-    'jsx-quotes': [1, 'prefer-single'],      // Single quotes on JSX components
-    'object-curly-spacing': [1, "never"],
-    'react/prop-types': 1,                   // Warn about missing prop types
-    'react/react-in-jsx-scope': 2,           // Require React be in scope when authoring JSX
-    'react/jsx-uses-react': 2,               // React doesn't have to be used explicitly b/c JSX
-    'react/jsx-uses-vars': 2,                // Prevent no-unused-vars issues with JSX components
+    "semi": [1, "always"],                   // Use semicolons
+    "comma-dangle": [1, "always-multiline"], // Use semicolons
+    "jsx-quotes": [1, "prefer-single"],      // Single quotes on JSX components
+    "object-curly-spacing": [1, "never"],
+    "quotes": [2, "double", "avoid-escape"],
+    "react/prop-types": 1,                   // Warn about missing prop types
+    "react/react-in-jsx-scope": 2,           // Require React be in scope when authoring JSX
+    "react/jsx-uses-react": 2,               // React doesn"t have to be used explicitly b/c JSX
+    "react/jsx-uses-vars": 2,                // Prevent no-unused-vars issues with JSX components
+    "jsx-quotes": [2, "prefer-double"],
   },
 }
--- client/components/App.js
+++ client/components/App.js
@@ -1,20 +1,37 @@
-import React, {PropTypes} from 'react';
-import classnames from 'classnames/bind';
-import 'normalize.css';
+import React, {PropTypes} from "react";
+import classnames from "classnames/bind";
+import "normalize.css";
 
 // Using CSS Modules so we assign the styles to a variable
-import s from './App.styl';
+import s from "./App.styl";
 const cx = classnames.bind(s);
-import axios from 'axios';
+import axios from "axios";
 
 // Favicon link is in the template, this just makes webpack package it up for us
-import './favicon.ico';
+import "./favicon.ico";
+
+const gifSelector = (status) => {
+  switch (status) {
+    case "Yep. Go outside and catch some!":
+      return "https://m.popkey.co/6abb47/Jmw09.gif";
+    case "Yep, but the servers are struggling :-(":
+      return "https://media.giphy.com/media/PiiQ5B1XxxiX6/giphy.gif";
+    case "Nope, servers are down! Go back to work.":
+      return "https://media.giphy.com/media/R15WrVMPBakLK/giphy.gif";
+    case "Error! Probably not a good sign, but try again.":
+      return "http://lovelace-media.imgix.net/uploads/186/c9e0a0f0-026a-0133-4582-0a2ca390b447.gif?";
+    case "checking status":
+      return "";
+    default:
+      return "https://66.media.tumblr.com/50812e936f6118e3577c990c2bca4341/tumblr_mwpotprvIW1rjenv2o1_500.gif";
+  }
+};
 
 export class Home extends React.Component {
   constructor(props) {
     super(props);
     this.setStatus = this.setStatus.bind(this);
-    this.state = {status: 'checking status'};
+    this.state = {status: "checking status"};
   }
 
   componentDidMount() {
@@ -31,10 +48,10 @@ export class Home extends React.Component {
   }
 
   fetchStatus() {
-    // const url = 'http://localhost:5000/?json=true';
-    const url = 'https://is-pokemon-go-up-api-spncenggln.now.sh/?json=true';
+    // const url = "http://localhost:5000/?json=true";
+    const url = "https://is-pokemon-go-up-api-gyzixdmrat.now.sh/?json=true";
     const init = {
-      method: 'GET',
+      method: "GET",
       headers: {},
     };
     return axios.get(url, init)
@@ -44,15 +61,18 @@ export class Home extends React.Component {
     })
     .catch((error) => {
       console.log(error);
-      return 'Looks like our Server is failed us';
+      return "Looks like our Server is failed us";
     });
   }
 
   render() {
+    const {status} = this.state;
+
     return (
-      <div className='Main'>
-        <h2 id='title'>Is Pokémon Go Up?</h2>
-        {this.state.status}
+      <div className="Main">
+        <h2>Is Pokémon Go Up?</h2>
+        {status}
+        <img src={gifSelector(status)} />
       </div>
     );
   }
@@ -61,8 +81,9 @@ export class Home extends React.Component {
 export class NotFound extends React.Component {
   render() {
     return (
-      <div className={cx('page')}>
-        <h4>Not found</h4>
+      <div className={cx("page")}>
+        <h2>Page not found</h2>
+        <img src={gifSelector()} />
       </div>
     );
   }
@@ -74,7 +95,7 @@ export class App extends React.Component {
   }
   render() {
     return (
-      <div className={cx('App')}>
+      <div className={cx("App")}>
         {this.props.children}
       </div>
     );
